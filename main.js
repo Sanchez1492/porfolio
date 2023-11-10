d24f616e67869dae529f56e39f9f705088d76d96
Sebastian Sanchez <dev.sebastian.sanchez@gmail.com>
2023-11-10 13:38:11 -0300

    added proyect slider

--- main.js
+++ main.js
@@ -14,3 +14,27 @@ closeMenu.addEventListener('click', () => {
     closeMenu.style.display = 'none';
     menu.style.right = '-100%'
 })
+
+var swiperProjects = new Swiper(".projects__container", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    breakpoints: {
+        // when window width is >= 320px
+        380: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
