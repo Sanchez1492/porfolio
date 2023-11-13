cd455f73a4fc232449d2e78aa11dfa2124eb5977
Sebastian Sanchez <dev.sebastian.sanchez@gmail.com>
2023-11-13 17:36:18 -0300

    Final version completed

--- main.js
+++ main.js
@@ -6,14 +6,22 @@ const closeMenu = document.querySelector('#close-menu-btn')
 function toggleMenu() {
     openMenu.style.display = 'none';
     closeMenu.style.display = 'block';
-    menu.style.right = '0'
+    menu.style.display = 'flex'
 }
 
 closeMenu.addEventListener('click', () => {
     openMenu.style.display = 'block';
     closeMenu.style.display = 'none';
-    menu.style.right = '-100%'
+    menu.style.display = 'none';
 })
+
+if (window.innerWidth < 768) {
+  menu.addEventListener('click', () => {
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+    menu.style.display = 'none';
+})
+}
 
 var swiperProjects = new Swiper(".projects__container", {
     slidesPerView: 1,
@@ -23,12 +31,12 @@ var swiperProjects = new Swiper(".projects__container", {
     centeredSlides: true,
     breakpoints: {
         // when window width is >= 320px
-        380: {
+        668: {
           slidesPerView: 2,
           spaceBetween: 20
         },
         // when window width is >= 480px
-        768: {
+        1027: {
           slidesPerView: 3,
           spaceBetween: 30
         },
@@ -37,4 +45,10 @@ var swiperProjects = new Swiper(".projects__container", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.body.addEventListener("scroll", function() {
+      document.body.scrollLeft = 0;
+    });
   });
